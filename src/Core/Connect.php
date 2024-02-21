d0503c09cb0dff6a54a04cdea87f0d0ee375a8bb
derrick921213 <dlin12457@gmail.com>
2024-02-21 17:12:22 +0800

    修正資料庫連線utf8

--- src/Core/Connect.php
+++ src/Core/Connect.php
@@ -25,6 +25,7 @@ class Connect {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
             ];
             $this->db_handle = new PDO($dsn, $this->db_user, $this->db_pass, $options);
         } catch (PDOException $e) {
