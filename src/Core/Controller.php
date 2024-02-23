75492bb9d78950fe8e8289d3caaf7e2bcf0bd382
derrick921213 <dlin12457@gmail.com>
2024-02-23 20:28:23 +0800

    register page done

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -1,33 +1,42 @@
 <?php
-
 namespace Exam\Core;
-
 use Exam\Core\Connect;
-
-class Controller
-{
+class Controller {
     private $handler = null;
     private $need_tables = [
-        "Users" => "CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(255) NOT NULL)",
+        "Users" => "CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY,`username` VARCHAR(50) NOT NULL UNIQUE,`password` VARCHAR(255) NOT NULL)",
     ];
-    public function __construct()
-    {
+    public function __construct() {
         $connect = new Connect();
         $this->handler = $connect->getHandler();
-        if (!isset($this->handler)) die("Can't get DB handler");
+        if(!isset($this->handler)) die("Can't get DB handler");
         $this->init_Table();
     }
-    private function init_Table()
-    {
-        foreach ($this->need_tables as $table => $val) {
-            if (!$this->table_Exists($table)) {
+    private function init_Table(){
+        foreach($this->need_tables as $table => $val){
+            if (!$this->table_Exists($table)){
                 $this->handler->exec($val);
             }
         }
     }
-    private function table_Exists($table)
-    {
+    private function table_Exists($table){
         $stmt = $this->handler->query("SHOW TABLES LIKE '$table'");
         return !($stmt->rowCount() == 0);
+    }
+    public function insert_User(string $user,string $password){
+        $sql = "INSERT INTO Users (`username`,`password`) VALUES (?,?)";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$user, $password]);
+        if (!$ret){
+            $errorInfo = $stmt->errorInfo();
+            die("SQL 錯誤：" . $errorInfo[2]);
+        }
+    }
+    public function check_User(string $user){
+        $sql = "SELECT `username` from `Users` WHERE `username` = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$user]);
+        if (!$ret) return false;
+        return $stmt->fetch();
     }
 }
--- src/Pages/register.php
+++ src/Pages/register.php
@@ -3,6 +3,7 @@
 namespace Exam\Pages;
 
 use Exam\Pages\twigLoader;
+use Exam\Core\Controller;
 
 class Register
 {
@@ -15,11 +16,13 @@ class Register
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $user = $_POST["user"] ?? '';
             $pass = $_POST["password"] ?? '';
-            $this->checkUser($user, $pass);
+            $this->register($user, $pass);
+            header('Location: /login');
         } else new twigLoader(__FILE__, false, $this->OPTIONS);
     }
-    private function checkUser(string $username, string $password)
-    {
-        var_dump($username, $password);
+    private function register(string $user,string $pass){
+        $controller = new Controller();
+        $password = password_hash($pass, PASSWORD_DEFAULT);
+        if (!$controller->check_User($user)) $controller->insert_User($user, $password);
     }
 }
