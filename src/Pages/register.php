74f14c5f0d3ad844dcb4aa653466feb3039cec2f
derrick921213 <dlin12457@gmail.com>
2024-02-23 17:51:15 +0800

    修正register.php

--- src/Pages/register.php
+++ src/Pages/register.php
@@ -6,7 +6,15 @@ class Register {
         "title" => "Add Your NID",
         "formTitle" => "FCU Test SignUp"
     ];
-    public function __construct(){
-        new twigLoader(__FILE__,false,$this->OPTIONS);
+    public function __construct() {
+        if (isset($_POST["user"]) && isset($_POST["password"])){
+            $user = $_POST["user"] ?? '';
+            $pass = $_POST["password"] ?? '';
+            $this->checkUser($user, $pass);
+        }
+        else new twigLoader(__FILE__,false,$this->OPTIONS);
+    }
+    private function checkUser(string $username, string $password) {
+        var_dump($username,$password);
     }
 }
