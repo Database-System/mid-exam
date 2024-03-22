04e20268b2fb4a41af3dd57a737b86c968e470cd
derrick921213 <dlin12457@gmail.com>
2024-03-22 16:29:00 +0800

    DB Create

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -8,10 +8,46 @@ class Controller
 {
     private $handler = null;
     private $need_tables = [
-        "Users" => "CREATE TABLE Users (`id` INT AUTO_INCREMENT PRIMARY KEY,`username` VARCHAR(50) NOT NULL UNIQUE,`password` VARCHAR(255) NOT NULL)",
-        // "Courses" => "CREATE TABLE Courses (`id` INT AUTO_INCREMENT PRIMARY KEY,`C_id` INT NOT NULL UNIQUE,`C_name` VARCHAR(255) NOT NULL,`C_point` INT NOT NULL)",
-        // //time table &time slot
-        // "Time_slot"=>"CREATE TABLE slot (`id` INT AUTO_INCREMENT PRIMARY KEY, `day` , `start_time` INT NOT NULL, `start_time` INT NOT NULL)",
+        "Users" => "CREATE TABLE `Users` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `username` varchar(50) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `Total_credits` int(10) UNSIGNED NOT NULL DEFAULT 0,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `username_UNIQUE` (`username`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
+
+        "TimeSlot" => "CREATE TABLE `TimeSlot` (
+            `time_slot_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `day` VARCHAR(255) NOT NULL,
+            `start_time` TIME NOT NULL,
+            `end_time` TIME NOT NULL,
+            PRIMARY KEY (`time_slot_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
+
+        "Course" => "CREATE TABLE `Course` (
+            `ID` INT(10) UNSIGNED NOT NULL,
+            `Name` VARCHAR(255) NOT NULL,
+            `Credits` INT(10) UNSIGNED NOT NULL,
+            PRIMARY KEY (`ID`),
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
+
+        "CourseTimeSlots" => "CREATE TABLE CourseTimeSlots (
+            Course_ID INT(10) UNSIGNED NOT NULL,
+            Time_Slot_ID INT(10) UNSIGNED NOT NULL,
+            PRIMARY KEY (Course_ID, Time_Slot_ID),
+            FOREIGN KEY (Course_ID) REFERENCES Course(ID),
+            FOREIGN KEY (Time_Slot_ID) REFERENCES TimeSlot(time_slot_id)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
+
+        "TimeTable" => "CREATE TABLE `TimeTable` (
+            `course_ID` INT(10) UNSIGNED NOT NULL,
+            `time_slot_id` INT(10) UNSIGNED NOT NULL,
+            `user_id` INT(11) NOT NULL,
+            FOREIGN KEY (`course_ID`) REFERENCES `Course`(`ID`),
+            FOREIGN KEY (`time_slot_id`) REFERENCES `TimeSlot`(`time_slot_id`),
+            FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
     ];
     public function __construct()
     {
