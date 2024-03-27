2162b95dbcd1ad0a0811c141507c31fa9c23d111
derrick921213 <dlin12457@gmail.com>
2024-03-27 10:22:02 +0800

    fix command.cmd to .ps1

---
+++ bin/command.ps1
@@ -0,0 +1,45 @@
+function Invoke-DockerCompose {
+    param (
+        [string]$command,
+        [string]$additionalArgs = ""
+    )
+
+    $dir = Split-Path $script:MyInvocation.MyCommand.Path -Parent
+    $dir = Resolve-Path "$dir\.."
+    Set-Location $dir
+
+    docker compose $command $additionalArgs
+}
+
+$scriptName = $MyInvocation.MyCommand.Name
+switch ($args[0]) {
+    "start" {
+        Invoke-DockerCompose -command "up --build -d"
+    }
+    "stop" {
+        Invoke-DockerCompose -command "down"
+    }
+    "restart" {
+        Invoke-DockerCompose -command "restart"
+    }
+    "test" {
+        $testName = $args[1]
+        Invoke-DockerCompose -command "run --build --rm server ./vendor/bin/phpunit tests/$testName.php"
+    }
+    "composer" {
+        $additionalArgs = $args[1..$args.Length] -join " "
+        $dir = Split-Path $script:MyInvocation.MyCommand.Path -Parent
+        $dir = Resolve-Path "$dir\.."
+        docker run --rm -it -v "${dir}:/app" composer $additionalArgs
+    }
+    "watch" {
+        Invoke-DockerCompose -command "watch"
+    }
+    "ps" {
+        Invoke-DockerCompose -command "ps"
+    }
+    default {
+        Write-Host "Usage: $scriptName {start|stop|restart|test|composer|watch|ps}"
+        exit 1
+    }
+}
