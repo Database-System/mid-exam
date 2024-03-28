25fea5ac2f892e79d0d2b646b86431797e1e308a
derrick921213 <dlin12457@gmail.com>
2024-03-29 05:14:06 +0800

    Test

--- tests/Hello_Test.php
+++ tests/Hello_Test.php
@@ -3,14 +3,20 @@ require_once './src/hello.php';
 
 class Hello_Test extends PHPUnit\Framework\TestCase
 {
- public function testOutput()
- {
-    // Capture the output of hello.php
-    ob_start();
-    include './src/hello.php';
-    $output = ob_get_clean();
+   public function testOutput()
+   {
+      // Capture the output of hello.php
+      ob_start();
+      include './src/hello.php';
+      $output = ob_get_clean();
 
-    // Assert that the output is "Hello, Docker!"
-    $this->assertEquals("Hello, Docker!", $output);
- }
+      // Assert that the output is "Hello, Docker!"
+      $this->assertEquals("10Hello, Docker!", $output);
+   }
+   public function testOutput1()
+   {
+      $this->markTestSkipped(
+         'This test has not been implemented yet.'
+      );
+   }
 }
