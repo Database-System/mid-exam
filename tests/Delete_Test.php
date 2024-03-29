addfdb06eae6845a471b11b3fba17133462a6afb
MLLSK <yyes911987@gmail.com>
2024-03-29 23:08:14 +0800

    insert

--- tests/Delete_Test.php
+++ tests/Delete_Test.php
@@ -11,22 +11,24 @@ class Delete_Test extends TestCase
         self::$controller = new Controller();
     }
 
-    public static function tearDownAfterClass(): void
-    {
-        self::$controller->cleanupTestData();
-    }
-
     public function testDeleteData()
     {
         // Delete test data
         self::$controller->delete_TimeTable(1312, 1, 1);
         self::$controller->delete_TimeTable(1312, 2, 1);
-
+        
         self::$controller->delete_CourseTimeSlots(1312, 1);
         self::$controller->delete_CourseTimeSlots(1312, 2);
         self::$controller->delete_CourseTimeSlots(1314, 3);
         self::$controller->delete_CourseTimeSlots(1314, 4);
-
+        
+        self::$controller->delete_Course(1312,"System_Program", 3);
+        self::$controller->delete_Course(1314, "Probability_and_Statistics", 3);
+        
+        self::$controller->delete_TimeSlot("Monday", "08:00:00", "10:00:00");
+        self::$controller->delete_TimeSlot("Wedensday", "11:10:00", "12:00:00");
+        self::$controller->delete_TimeSlot("Monday", "13:10:00", "15:00:00");
+        self::$controller->delete_TimeSlot("Tuesday", "11:10:00", "12:00:00");
+        self::$controller->delete_TimeSlot("Monday", "15:10:00", "17:00:00");
     }
 }
-?>
