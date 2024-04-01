ac5aa880da2cceab8ffb80d215deda2a7ddc5a69
Huahua1222 <monkey12521>
2024-04-01 23:58:07 +0800

    test

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -17,7 +17,7 @@ class Controller
             PRIMARY KEY (`id`),
             UNIQUE KEY `username_UNIQUE` (`username`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-    
+
         "TimeSlot" => "CREATE TABLE `TimeSlot` (
             `time_slot_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
             `day` VARCHAR(255) NOT NULL,
@@ -25,7 +25,7 @@ class Controller
             `end_time` TIME NOT NULL,
             PRIMARY KEY (`time_slot_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-    
+
         "Course" => "CREATE TABLE `Course` (
             `ID` INT(10) UNSIGNED NOT NULL,
             `Name` VARCHAR(255) NOT NULL,
@@ -35,7 +35,7 @@ class Controller
             `MaxPeople` INT(10) UNSIGNED NOT NULL DEFAULT 0,
             PRIMARY KEY (`ID`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-    
+
         "CourseTimeSlots" => "CREATE TABLE `CourseTimeSlots` (
             `Course_ID` INT(10) UNSIGNED NOT NULL,
             `Time_Slot_ID` INT(10) UNSIGNED NOT NULL,
@@ -43,7 +43,7 @@ class Controller
             FOREIGN KEY (`Course_ID`) REFERENCES `Course`(`ID`),
             FOREIGN KEY (`Time_Slot_ID`) REFERENCES `TimeSlot`(`time_slot_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-    
+
         "TimeTable" => "CREATE TABLE `TimeTable` (
             `course_ID` INT(10) UNSIGNED NOT NULL,
             `time_slot_id` INT(10) UNSIGNED NOT NULL,
@@ -57,9 +57,10 @@ class Controller
     {
         $connect = new Connect();
         $this->handler = $connect->getHandler();
-        if (!isset($this->handler)) die("Can't get DB handler");
+        if (!isset($this->handler))
+            die("Can't get DB handler");
         $this->init_Table();
-    }    
+    }
     private function init_Table(): void
     {
         foreach ($this->need_tables as $table => $val) {
@@ -72,13 +73,14 @@ class Controller
     {
         $stmt = $this->handler->query("SHOW TABLES LIKE '$table'");
         return !($stmt->rowCount() == 0);
-    }    
-    public function insert_User(string $user, string $password): bool | array 
+    }
+    public function insert_User(string $user, string $password): bool|array
     {
         $sql = "INSERT INTO Users (`username`,`password`) VALUES (?,?)";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$user, $password]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return $stmt->fetch();
     }
     public function check_User(string $user): bool|array
@@ -86,43 +88,48 @@ class Controller
         $sql = "SELECT * from `Users` WHERE `username` = ?";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$user]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return $stmt->fetch();
     }
 
-    public function insert_TimeSlot(string $day,string $start_time,string $end_time)
+    public function insert_TimeSlot(string $day, string $start_time, string $end_time)
     {
         $sql = "INSERT INTO TimeSlot (`day`,`start_time`,`end_time`) VALUES (?,?,?)";
         $stmt = $this->handler->prepare($sql);
-        $ret = $stmt->execute([$day,$start_time,$end_time]);
-        if (!$ret) return false;
+        $ret = $stmt->execute([$day, $start_time, $end_time]);
+        if (!$ret)
+            return false;
         return true;
     }
 
-    public function insert_Course(int $ID,string $Name,int $Credits)
+    public function insert_Course(int $ID, string $Name, int $Credits)
     {
         $sql = "INSERT INTO Course (`ID`,`Name`,`Credits`) VALUES (?,?,?)";
         $stmt = $this->handler->prepare($sql);
-        $ret = $stmt->execute([$ID,$Name,$Credits]);
-        if (!$ret) return false;
+        $ret = $stmt->execute([$ID, $Name, $Credits]);
+        if (!$ret)
+            return false;
         return true;
     }
 
-    public function insert_CourseTimeSlots(int $Course_ID,int $Time_Slot_ID)
+    public function insert_CourseTimeSlots(int $Course_ID, int $Time_Slot_ID)
     {
         $sql = "INSERT INTO CourseTimeSlots (`Course_ID`,`Time_Slot_ID`) VALUES (?,?)";
         $stmt = $this->handler->prepare($sql);
-        $ret = $stmt->execute([$Course_ID,$Time_Slot_ID]);
-        if (!$ret) return false;
+        $ret = $stmt->execute([$Course_ID, $Time_Slot_ID]);
+        if (!$ret)
+            return false;
         return true;
     }
 
-    public function insert_TimeTable(int $course_ID,int $time_slot_id,int $user_id)
+    public function insert_TimeTable(int $course_ID, int $time_slot_id, int $user_id)
     {
         $sql = "INSERT INTO TimeTable (`course_ID`,`time_slot_id`,`user_id`) VALUES (?,?,?)";
         $stmt = $this->handler->prepare($sql);
-        $ret = $stmt->execute([$course_ID,$time_slot_id,$user_id]);
-        if (!$ret) return false;
+        $ret = $stmt->execute([$course_ID, $time_slot_id, $user_id]);
+        if (!$ret)
+            return false;
         return true;
     }
 
@@ -131,7 +138,8 @@ class Controller
         $sql = "DELETE FROM `TimeSlot` WHERE `day` = ? AND `start_time` = ? AND `end_time` = ?";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$day, $start_time, $end_time]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return true;
     }
 
@@ -140,7 +148,8 @@ class Controller
         $sql = "DELETE FROM `Course` WHERE `ID` = ? AND `Name` = ? AND `Credits` = ?";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$ID, $Name, $Credits]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return true;
     }
 
@@ -150,7 +159,8 @@ class Controller
         $sql = "DELETE FROM `TimeTable` WHERE `course_ID` = ? AND `time_slot_id` = ? AND `user_id` = ?";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$course_id, $time_slot_id, $user_id]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return true;
     }
 
@@ -159,8 +169,48 @@ class Controller
         $sql = "DELETE FROM `CourseTimeSlots` WHERE `course_ID` = ? AND `time_Slot_ID` = ?";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$course_id, $time_slot_id]);
-        if (!$ret) return false;
+        if (!$ret)
+            return false;
         return true;
     }
-    
+    //search
+    public function search_User_TimeTable(string $username, int $courseID):bool
+    {
+        $user = $this->check_User( $username);
+
+        $course = $this->check_Course($courseID);
+
+        $sql = "SELECT * FROM TimeTable WHERE user_id = ? AND course_ID = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$user['id'], $courseID]);
+        if (!$ret)
+            return false;
+        return true;
+    }
+    private function check_Course(int $courseID):bool|array
+    {
+        $sql = "SELECT * FROM Course WHERE ID = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$courseID]);
+        if (!$ret)
+            return false;
+        return $stmt->fetch();
+
+    }
+
+    //update
+    public function display_User_TimeTable(string $username):bool|array
+    {
+        $sql = "SELECT Course.Name, TimeSlot.day, TimeSlot.start_time, TimeSlot.end_time
+                FROM Users
+                INNER JOIN TimeTable ON Users.id = TimeTable.user_id
+                INNER JOIN Course ON TimeTable.course_ID = Course.ID
+                INNER JOIN TimeSlot ON TimeTable.time_slot_id = TimeSlot.time_slot_id
+                WHERE Users.username = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$username]);
+        if (!$ret)
+            return false;
+        return $stmt->fetchAll();
+    }
 }
---
+++ tests/Controller_update_Test.php
@@ -0,0 +1,132 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Exam\Core\Controller;
+use Exam\Core\Connect;
+require_once './src/Core/Controller.php';
+
+class Controller_update_Test extends TestCase{
+    protected static $controller;
+    private static $handler = null;
+
+    public static function setUpBeforeClass():void{
+        $connect=new Connect();
+        self::$handler = $connect->getHandler();
+    }
+    protected function setUp():void
+    {
+        $this->delete_table();
+        self::$controller = new Controller();
+        $this->insert_Courses();
+        $this->insert_timeslot();
+        $this->insert_timetable();
+        $this->insert_coursetimeslots();
+    }
+    private function delete_table()
+    {
+        $need_table=["TimeTable","CourseTimeSlots","TimeSlot","Course"];
+        foreach($need_table as $table)
+        {
+            if(!$this->table_Exists($table))
+            {
+                continue;
+            }
+            self::$handler->exec("DROP TABLE `$table`");
+        }
+    }
+    private function table_Exists($table)
+    {
+        $stmt = self::$handler->query("SHOW TABLES LIKE '$table'");
+        return !($stmt->rowCount() == 0);
+    }
+    private function insert_Courses(){
+        $sql = "INSERT INTO Course (ID, Name, Credits) VALUES 
+            (1312, '系統程式', 3),
+            (1314, '機率與統計', 3),
+            (1313, '資料庫系統', 3),
+            (2864, '日文(一)', 2),
+            (1311, '班級活動', 0),
+            (1324, 'Web程式設計', 3),
+            (2990, '漢字之美', 2),
+            (1365, '程式設計與問題解決', 2),
+            (2809, '華語教材教法', 2),
+            (3320, '大學精進英文(二)中級', 2)";
+        $stmt = self::$handler->prepare($sql);
+        $ret = $stmt->execute();
+        if (!$ret) return false;
+    }
+    private function insert_timeslot(){
+        $sql = "INSERT INTO TimeSlot (time_slot_id, day, start_time,end_time) VALUES 
+            (1, '星期一', '10:10:00', '12:00:00'),
+            (2, '星期三', '11:10:00', '12:00:00'),
+            (3, '星期一', '13:10:00', '15:00:00'),
+            (4, '星期二', '11:10:00', '12:00:00'),
+            (5, '星期一', '15:10:00', '17:00:00'),
+            (6, '星期二', '10:10:00', '11:00:00'),
+            (7, '星期二', '13:10:00', '15:00:00'),
+            (8, '星期二', '16:10:00', '17:00:00'),
+            (9, '星期三', '08:10:00', '11:00:00'),
+            (10, '星期四', '10:10:00', '12:00:00'),
+            (11, '星期四', '13:10:00', '15:00:00'),
+            (12, '星期四', '18:30:00', '20:15:00'),
+            (13, '星期五', '08:10:00', '10:00:00')";
+        $stmt = self::$handler->prepare($sql);
+        $ret = $stmt->execute();
+        if (!$ret) return false;
+    }
+
+    private function insert_coursetimeslots(){
+        $sql = "INSERT INTO CourseTimeSlots (Course_ID, Time_Slot_ID) VALUES 
+            (1312,1),(1312,2),
+            (1314,3),(1314,4),
+            (1313,5),(1313,6),
+            (2864,7),
+            (1311,8),
+            (1324,9),
+            (2990,10),
+            (1365,11),
+            (2809,12),
+            (3320,13)";
+        $stmt = self::$handler->prepare($sql);
+        $ret = $stmt->execute();
+        if (!$ret) return false;
+}
+
+    private function insert_timetable(){
+        $sql = "INSERT INTO TimeTable (course_ID, time_slot_id, user_id) VALUES 
+            (1312,1,1),(1312,2,1),
+            (2864,7,1),
+            (1311,8,1),
+            (2809,12,1)";
+        $stmt = self::$handler->prepare($sql);
+        $ret = $stmt->execute();
+        if (!$ret) return false;
+    }
+
+    public function testSearch(){
+        $result1= self::$controller->search_User_TimeTable('test_user',1312);
+        $this->assertTrue($result1);
+
+        $result2= self::$controller->search_User_TimeTable('test_user',1311);
+        $this->assertTrue($result2);
+    }
+    public function testDisplayUserTimeTable(){
+        $result = self::$controller->display_User_TimeTable('test_user');
+        //echo var_dump($result);
+        $expected = [
+            ["Name"=>"系統程式","day"=>"星期一","start_time"=>"10:10:00","end_time"=>"12:00:00"],
+            ["Name"=>"系統程式","day"=>"星期三","start_time"=>"11:10:00","end_time"=>"12:00:00"],
+            ["Name"=>"日文(一)","day"=>"星期二","start_time"=>"13:10:00","end_time"=>"15:00:00"],
+            ["Name"=>"班級活動","day"=>"星期二","start_time"=>"16:10:00","end_time"=>"17:00:00"],
+            ["Name"=>"華語教材教法","day"=>"星期四","start_time"=>"18:30:00","end_time"=>"20:15:00"]
+        ];
+        //echo var_dump($expected);
+        $this->assertEquals($expected,$result);
+    }
+    public function tearDown(): void { 
+        $this->delete_table();
+    }
+    public static function tearDownAfterClass(): void{
+        self::$controller=null;
+        self::$handler=null;
+    }
+}
