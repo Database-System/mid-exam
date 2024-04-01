09cfba51ab28a2e8100110e403b0c009167bc86e
MLLSK <126126748+MLLSK@users.noreply.github.com>
2024-04-01 21:22:15 +0800

    create insert tests
    * test
    
    * Insert_and_Delete
    
    * FixTest_Allcomplete
    
    * fix_get_private_handler
    
    * second fix
    
    ---------
    
    Co-authored-by: derrick921213 <60911408+derrick921213@users.noreply.github.com>

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -17,7 +17,7 @@ class Controller
             PRIMARY KEY (`id`),
             UNIQUE KEY `username_UNIQUE` (`username`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-
+    
         "TimeSlot" => "CREATE TABLE `TimeSlot` (
             `time_slot_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
             `day` VARCHAR(255) NOT NULL,
@@ -25,7 +25,7 @@ class Controller
             `end_time` TIME NOT NULL,
             PRIMARY KEY (`time_slot_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-
+    
         "Course" => "CREATE TABLE `Course` (
             `ID` INT(10) UNSIGNED NOT NULL,
             `Name` VARCHAR(255) NOT NULL,
@@ -35,15 +35,15 @@ class Controller
             `MaxPeople` INT(10) UNSIGNED NOT NULL DEFAULT 0,
             PRIMARY KEY (`ID`),
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-
-        "CourseTimeSlots" => "CREATE TABLE CourseTimeSlots (
-            Course_ID INT(10) UNSIGNED NOT NULL,
-            Time_Slot_ID INT(10) UNSIGNED NOT NULL,
-            PRIMARY KEY (Course_ID, Time_Slot_ID),
-            FOREIGN KEY (Course_ID) REFERENCES Course(ID),
-            FOREIGN KEY (Time_Slot_ID) REFERENCES TimeSlot(time_slot_id)
+    
+        "CourseTimeSlots" => "CREATE TABLE `CourseTimeSlots` (
+            `Course_ID` INT(10) UNSIGNED NOT NULL,
+            `Time_Slot_ID` INT(10) UNSIGNED NOT NULL,
+            PRIMARY KEY (`Course_ID`, `Time_Slot_ID`),
+            FOREIGN KEY (`Course_ID`) REFERENCES `Course`(`ID`),
+            FOREIGN KEY (`Time_Slot_ID`) REFERENCES `TimeSlot`(`time_slot_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
-
+    
         "TimeTable" => "CREATE TABLE `TimeTable` (
             `course_ID` INT(10) UNSIGNED NOT NULL,
             `time_slot_id` INT(10) UNSIGNED NOT NULL,
@@ -52,7 +52,7 @@ class Controller
             FOREIGN KEY (`time_slot_id`) REFERENCES `TimeSlot`(`time_slot_id`),
             FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
-    ];    
+    ];
     public function __construct()
     {
         $connect = new Connect();
@@ -78,10 +78,8 @@ class Controller
         $sql = "INSERT INTO Users (`username`,`password`) VALUES (?,?)";
         $stmt = $this->handler->prepare($sql);
         $ret = $stmt->execute([$user, $password]);
-        if (!$ret) {
-            $errorInfo = $stmt->errorInfo();
-            die("SQL 錯誤：" . $errorInfo[2]);
-        }
+        if (!$ret) return false;
+        return $stmt->fetch();
     }
     public function check_User(string $user): bool|array
     {
@@ -91,4 +89,78 @@ class Controller
         if (!$ret) return false;
         return $stmt->fetch();
     }
+
+    public function insert_TimeSlot(string $day,string $start_time,string $end_time)
+    {
+        $sql = "INSERT INTO TimeSlot (`day`,`start_time`,`end_time`) VALUES (?,?,?)";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$day,$start_time,$end_time]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function insert_Course(int $ID,string $Name,int $Credits)
+    {
+        $sql = "INSERT INTO Course (`ID`,`Name`,`Credits`) VALUES (?,?,?)";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$ID,$Name,$Credits]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function insert_CourseTimeSlots(int $Course_ID,int $Time_Slot_ID)
+    {
+        $sql = "INSERT INTO CourseTimeSlots (`Course_ID`,`Time_Slot_ID`) VALUES (?,?)";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$Course_ID,$Time_Slot_ID]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function insert_TimeTable(int $course_ID,int $time_slot_id,int $user_id)
+    {
+        $sql = "INSERT INTO TimeTable (`course_ID`,`time_slot_id`,`user_id`) VALUES (?,?,?)";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$course_ID,$time_slot_id,$user_id]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function delete_TimeSlot(string $day, string $start_time, string $end_time)
+    {
+        $sql = "DELETE FROM `TimeSlot` WHERE `day` = ? AND `start_time` = ? AND `end_time` = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$day, $start_time, $end_time]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function delete_Course(int $ID, string $Name, int $Credits)
+    {
+        $sql = "DELETE FROM `Course` WHERE `ID` = ? AND `Name` = ? AND `Credits` = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$ID, $Name, $Credits]);
+        if (!$ret) return false;
+        return true;
+    }
+
+
+    public function delete_TimeTable(int $course_id, int $time_slot_id, int $user_id)
+    {
+        $sql = "DELETE FROM `TimeTable` WHERE `course_ID` = ? AND `time_slot_id` = ? AND `user_id` = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$course_id, $time_slot_id, $user_id]);
+        if (!$ret) return false;
+        return true;
+    }
+
+    public function delete_CourseTimeSlots(int $course_id, int $time_slot_id)
+    {
+        $sql = "DELETE FROM `CourseTimeSlots` WHERE `course_ID` = ? AND `time_Slot_ID` = ?";
+        $stmt = $this->handler->prepare($sql);
+        $ret = $stmt->execute([$course_id, $time_slot_id]);
+        if (!$ret) return false;
+        return true;
+    }
+    
 }
