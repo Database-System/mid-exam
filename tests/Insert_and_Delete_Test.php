9960adef5249f2507616a951198adca63be4a699
MLLSK <126126748+MLLSK@users.noreply.github.com>
2024-04-04 18:25:12 +0800

    Renew insert and delete (#19)
    * add temp1
    
    * 修正目錄位置
    
    * 修正temp1
    
    * remove temp1
    
    * 新增dashboard的測試
    
    * 修正
    
    * add image
    
    * fix db schme
    
    * 添加型別
    
    * create insert tests
    
    * test
    
    * Insert_and_Delete
    
    * FixTest_Allcomplete
    
    * fix_get_private_handler
    
    * second fix
    
    ---------
    
    Co-authored-by: derrick921213 <60911408+derrick921213@users.noreply.github.com>
    
    * 修正return type
    
    * 修正web
    
    * fix tests
    
    * Renew_insert_and_delete
    
    ---------
    
    Co-authored-by: derrick921213 <dlin12457@gmail.com>
    Co-authored-by: Karylcode <164126332+Karylcode@users.noreply.github.com>
    Co-authored-by: derrick921213 <60911408+derrick921213@users.noreply.github.com>

---
+++ tests/Insert_and_Delete_Test.php
@@ -0,0 +1,91 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Exam\Core\Controller;
+use Exam\Core\Connect;
+require_once './src/Core/Controller.php';
+
+class Insert_and_Delete_Test extends TestCase
+{
+    protected static $controller;
+    private static $handler;
+
+    public static function setUpBeforeClass(): void
+    {
+        $connect = new Connect();
+        self::$handler = $connect->getHandler();
+
+    }
+    protected function setUp():void
+    {
+        $this->delete_table();
+        self::$controller = new Controller();
+    }
+    private function delete_table()
+    {
+        $need_table=["TimeTable","CourseTimeSlots","TimeSlot","Course"];
+        foreach($need_table as $table)
+        {
+            if(!$this->table_Exists($table))
+            {
+                continue;
+            }
+            self::$handler->exec("DROP TABLE `$table`");
+        }
+    }
+    private function table_Exists($table)
+    {
+        $stmt = self::$handler->query("SHOW TABLES LIKE '$table'");
+        return !($stmt->rowCount() == 0);
+    }
+    public function testInsertData()
+    {
+        // Insert test data and assert that each insertion is successful
+        $this->assertTrue(self::$controller->insert_TimeSlot("Monday", "08:00:00", "10:00:00"));
+        $this->assertTrue(self::$controller->insert_TimeSlot("Wednesday", "11:10:00", "12:00:00")); // Corrected spelling of "Wednesday"
+        $this->assertTrue(self::$controller->insert_TimeSlot("Monday", "13:10:00", "15:00:00"));
+        $this->assertTrue(self::$controller->insert_TimeSlot("Tuesday", "11:10:00", "12:00:00"));
+        $this->assertTrue(self::$controller->insert_TimeSlot("Monday", "15:10:00", "17:00:00"));
+        
+        $this->assertTrue(self::$controller->insert_Course(1312, "System_Program", "Computer_Science", 1, 3, 60));
+        $this->assertTrue(self::$controller->insert_Course(1314, "Probability_and_Statistics", "Computer_Science", 1, 3, 50));
+
+        $this->assertTrue(self::$controller->insert_CourseTimeSlots(1312, 1));
+        $this->assertTrue(self::$controller->insert_CourseTimeSlots(1312, 2));
+        $this->assertTrue(self::$controller->insert_CourseTimeSlots(1314, 3));
+        $this->assertTrue(self::$controller->insert_CourseTimeSlots(1314, 4));
+
+        $this->assertTrue(self::$controller->insert_TimeTable(1312, 1, 1));
+        $this->assertTrue(self::$controller->insert_TimeTable(1312, 2, 1));
+    }
+
+    public function testDeleteData()
+    {
+        // Delete test data and assert that each deletion is successful
+        $this->assertTrue(self::$controller->delete_TimeTable(1312, 1, 1));
+        $this->assertTrue(self::$controller->delete_TimeTable(1312, 2, 1));
+        
+        $this->assertTrue(self::$controller->delete_CourseTimeSlots(1312, 1));
+        $this->assertTrue(self::$controller->delete_CourseTimeSlots(1312, 2));
+        $this->assertTrue(self::$controller->delete_CourseTimeSlots(1314, 3));
+        $this->assertTrue(self::$controller->delete_CourseTimeSlots(1314, 4));
+        
+        $this->assertTrue(self::$controller->delete_Course(1312, "System_Program", "Computer_Science", 1, 3, 60));
+        $this->assertTrue(self::$controller->delete_Course(1314, "Probability_and_Statistics", "Computer_Science", 1, 3, 50));
+        
+        $this->assertTrue(self::$controller->delete_TimeSlot("Monday", "08:00:00", "10:00:00"));
+        $this->assertTrue(self::$controller->delete_TimeSlot("Wednesday", "11:10:00", "12:00:00")); // Corrected spelling
+        $this->assertTrue(self::$controller->delete_TimeSlot("Monday", "13:10:00", "15:00:00"));
+        $this->assertTrue(self::$controller->delete_TimeSlot("Tuesday", "11:10:00", "12:00:00"));
+        $this->assertTrue(self::$controller->delete_TimeSlot("Monday", "15:10:00", "17:00:00"));
+    }
+
+    protected function tearDown(): void
+    {
+        $this->delete_table();
+    }
+    public static function tearDownAfterClass(): void
+    {
+        self::$controller = null;
+        self::$handler = null;
+    }
+}
