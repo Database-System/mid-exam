25d513cf44da0d00d664e89dcdf50504b3414c21
Huahua1222 <monkey12521>
2024-04-16 23:42:28 +0800

    people number

--- tests/Controller_update_Test.php
+++ tests/Controller_update_Test.php
@@ -16,10 +16,10 @@ class Controller_update_Test extends TestCase{
         $this->delete_table();
         self::$controller = new Controller();
         $this->insert_Users();
-        $this->insert_Courses();
-        $this->insert_timeslot();
-        $this->insert_coursetimeslots();
-        $this->insert_timetable();
+        // $this->insert_Courses();
+        // $this->insert_timeslot();
+        // $this->insert_coursetimeslots();
+        // $this->insert_timetable();
     }
     private function delete_table()
     {
@@ -42,22 +42,22 @@ class Controller_update_Test extends TestCase{
         $password = password_hash("12345678", PASSWORD_DEFAULT);
         self::$controller->insert_User("test_user",$password);
     }
-    private function insert_Courses(){
-        $sql = "INSERT INTO Course (ID, Name, Credits) VALUES 
-            (1312, '系統程式', 3),
-            (1314, '機率與統計', 3),
-            (1313, '資料庫系統', 3),
-            (2864, '日文(一)', 2),
-            (1311, '班級活動', 0),
-            (1324, 'Web程式設計', 3),
-            (2990, '漢字之美', 2),
-            (1365, '程式設計與問題解決', 2),
-            (2809, '華語教材教法', 2),
-            (3320, '大學精進英文(二)中級', 2)";
-        $stmt = self::$handler->prepare($sql);
-        $ret = $stmt->execute();
-        if (!$ret) return false;
-    }
+    // private function insert_Courses(){
+    //     $sql = "INSERT INTO Course (ID, Name, cls_name, dept,request , Credits,CurrentPeople,MaxPeople) VALUES 
+    //         (1312, '系統程式','資訊二乙','資訊系',1, 3,20,30),
+    //         (1314, '機率與統計','資訊二乙','資訊系',1, 3,30,30),
+    //         (1313, '資料庫系統','資訊二乙','資訊系',1, 3,40,30),
+    //         (2864, '日文(一)','資訊二乙','資訊系',0, 2,0,30),
+    //         (1311, '班級活動','資訊二乙','資訊系',1, 0,0,30),
+    //         (1324, 'Web程式設計','資訊二乙','資訊系',0, 3,0,30),
+    //         (2990, '漢字之美','資訊二乙','資訊系',0, 2,0,30),
+    //         (1365, '程式設計與問題解決','資訊二乙','資訊系',0, 2,0,30),
+    //         (2809, '華語教材教法','資訊二乙','資訊系',0, 2,0,30),
+    //         (3320, '大學精進英文(二)中級','資訊二乙','資訊系',1, 2,0,30)";
+    //     $stmt = self::$handler->prepare($sql);
+    //     $ret = $stmt->execute();
+    //     if (!$ret) return false;
+    // }
     private function insert_timeslot(){
         $sql = "INSERT INTO TimeSlot (time_slot_id, day, start_time,end_time) VALUES 
             (1, '星期一', '10:10:00', '12:00:00'),
@@ -106,43 +106,52 @@ class Controller_update_Test extends TestCase{
         if (!$ret) return false;
     }
 
-    public function testSearch(){
-        $result1= self::$controller->search_User_TimeTable('test_user',1312);
-        $this->assertTrue($result1);
+    // public function testSearch(){
+    //     $result1= self::$controller->search_User_TimeTable('test_user',1312);
+    //     $this->assertTrue($result1);
 
-        $result2= self::$controller->search_User_TimeTable('test_user',1311);
-        $this->assertTrue($result2);
-    }
-    public function testDisplayUserTimeTable(){
-        $result = self::$controller->display_User_TimeTable('test_user');
-        $expected = [
-            ["Name"=>"系統程式","day"=>"星期一","start_time"=>"10:10:00","end_time"=>"12:00:00"],
-            ["Name"=>"系統程式","day"=>"星期三","start_time"=>"11:10:00","end_time"=>"12:00:00"],
-            ["Name"=>"日文(一)","day"=>"星期二","start_time"=>"13:10:00","end_time"=>"15:00:00"],
-            ["Name"=>"班級活動","day"=>"星期二","start_time"=>"16:10:00","end_time"=>"17:00:00"],
-            ["Name"=>"華語教材教法","day"=>"星期四","start_time"=>"18:30:00","end_time"=>"20:15:00"]
-        ];
-        $this->assertEquals($expected,$result);
-    }
+    //     $result2= self::$controller->search_User_TimeTable('test_user',1311);
+    //     $this->assertTrue($result2);
+    // }
+    // public function testDisplayUserTimeTable(){
+    //     $result = self::$controller->display_User_TimeTable('test_user');
+    //     $expected = [
+    //         ["Name"=>"系統程式","day"=>"星期一","start_time"=>"10:10:00","end_time"=>"12:00:00"],
+    //         ["Name"=>"系統程式","day"=>"星期三","start_time"=>"11:10:00","end_time"=>"12:00:00"],
+    //         ["Name"=>"日文(一)","day"=>"星期二","start_time"=>"13:10:00","end_time"=>"15:00:00"],
+    //         ["Name"=>"班級活動","day"=>"星期二","start_time"=>"16:10:00","end_time"=>"17:00:00"],
+    //         ["Name"=>"華語教材教法","day"=>"星期四","start_time"=>"18:30:00","end_time"=>"20:15:00"]
+    //     ];
+    //     $this->assertEquals($expected,$result);
+    // }
     public function testUpdate(){
         
-        $result1=self::$controller->updateCourse(1312,'MaxPeople',30);
-        $this->assertTrue($result1);
-        $result3=self::$controller->updateCourse(1312,'Name','系統程式W');
-        $this->assertTrue($result3);
+        // $result1=self::$controller->updateCourse(1312,'MaxPeople',30);
+        // $this->assertTrue($result1);
+        // $result3=self::$controller->updateCourse(1312,'Name','系統程式W');
+        // $this->assertTrue($result3);
 
-        $courseInfo = $this->CourseInfo(1312);
-        $this->assertEquals(30, $courseInfo['MaxPeople']);
-        $courseInfo = $this->CourseInfo(1312);
-        $this->assertEquals('系統程式W', $courseInfo['Name']);
+        // $courseInfo = $this->CourseInfo(1312);
+        // $this->assertEquals(30, $courseInfo['MaxPeople']);
+        // $courseInfo = $this->CourseInfo(1312);
+        // $this->assertEquals('系統程式W', $courseInfo['Name']);
 
-        $result2=self::$controller->updateTimeSlots(6, '星期二', '10:30:00','11:00:00');
-        $this->assertTrue($result2);
-        $this->assertTrue(self::$controller->update_User_dept("test_user","Computer_Science"));
-        $this->assertTrue(self::$controller->update_User_TotalCerdits("test_user"));
+        // $result2=self::$controller->updateTimeSlots(6, '星期二', '10:30:00','11:00:00');
+        // $this->assertTrue($result2);
+        // $this->assertTrue(self::$controller->update_User_dept("test_user","Computer_Science"));
+        // $this->assertTrue(self::$controller->update_User_TotalCerdits("test_user"));
 
-        $result4=self::$controller->updateCourseTimeSlots(1312,3);
-        $this->assertTrue($result4);
+        // $result4=self::$controller->updateCourseTimeSlots(1312,3);
+        // $this->assertTrue($result4);
+
+        $result5=self::$controller->check_people_number(1312) ;
+        $this->assertTrue($result5);
+
+        $result6=self::$controller->check_people_number(1313) ;
+        $this->assertTrue($result6);
+
+        $result7=self::$controller->check_people_number(1314) ;
+        $this->assertTrue($result7);
     }
 
     public function CourseInfo(int $ID): bool|array
